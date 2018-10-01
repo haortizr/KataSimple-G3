a0bbe8aa304b813415583a8aed64ef4d73caad2f
Lady Pinzon <ljeraldynnepinzonc@gmail.com>
2018-09-30 21:20:59 -0500

    IT1- Se soluciona prueba de longitud con un elemento (VERDE)

--- KataSimpleTDD.py
+++ KataSimpleTDD.py
@@ -1,4 +1,6 @@
 class EstadisticaArreglo:
     def obtenerLongitud(self, cadenaString):
         if cadenaString == '':
-            return 0
+            return [0]
+        else:
+            return [int(cadenaString)]
--- TestNumeros.py
+++ TestNumeros.py
@@ -5,5 +5,6 @@ class TestLongitud(TestCase):
     def testEstadisticaArreglo(self):
         self.assertEqual(EstadisticaArreglo().obtenerLongitud(''),[0],"cadena vacia")
 
+
     def TestLongitudConNumero(self):
             self.assertEqual(EstadisticaArreglo().obtenerLongitud("1"), [1], "cadena con un numero")
