baaacfdcec2e11aaecf932e9909b8461870deed7
Lady Pinzon <ljeraldynnepinzonc@gmail.com>
2018-09-30 22:02:52 -0500

    IT2- Falla Prueba que devuelve el numero de elementos y el minimo(ROJO)

--- TestNumeros.py
+++ TestNumeros.py
@@ -5,13 +5,15 @@ class TestLongitud(TestCase):
     def testEstadisticaArreglo(self):
         self.assertEqual(EstadisticaArreglo().obtenerLongitud(''),[0],"cadena vacia")
 
-
-    def TestLongitudConNumero(self):
+    def testLongitudConNumero(self):
         self.assertEqual(EstadisticaArreglo().obtenerLongitud("1"), [1], "cadena con un numero")
 
-    def TestLongitudDosNumero(self):
+    def testLongitudDosNumero(self):
         self.assertEqual(EstadisticaArreglo().obtenerLongitud("1,2"), [2], "cadena con dos numero")
 
-    def TestLongitudNNumero(self):
+    def testLongitudNNumero(self):
         self.assertEqual(EstadisticaArreglo().obtenerLongitud("1,2,3,4"), [4], "cadena con cuatro numero")
         self.assertEqual(EstadisticaArreglo().obtenerLongitud("1,2,3,4,5"), [5], "cadena con cinco numero")
+
+    def testElementosMinMax(self):
+            self.assertEqual(EstadisticaArreglo().getArreglo(""), [0, None, None], "cadena vacia")
