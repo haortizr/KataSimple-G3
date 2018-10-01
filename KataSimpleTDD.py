7e9cea56a68e9e95575c7471fbb284de7226dffe
Lady Pinzon <ljeraldynnepinzonc@gmail.com>
2018-09-30 21:38:42 -0500

    IT1- Se soluciona prueba de longitud con dos elementos (VERDE)

--- KataSimpleTDD.py
+++ KataSimpleTDD.py
@@ -2,5 +2,7 @@ class EstadisticaArreglo:
     def obtenerLongitud(self, cadenaString):
         if cadenaString == '':
             return [0]
+        elif len(cadenaString) > 1:
+            return [len(cadenaString) - 1]
         else:
             return [int(cadenaString)]
