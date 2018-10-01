1d2a81e0eb707398173ebbf93d6d4493edc7a309
Lady Pinzon <ljeraldynnepinzonc@gmail.com>
2018-09-30 23:22:30 -0500

    IT3 - Falla Prueba que devuelve el número de elementos y el minimo, el maximo con cadena de N elementos (ROJO)

--- TestNumeros.py
+++ TestNumeros.py
@@ -5,4 +5,4 @@ from KataSimpleTDD import EstadisticaArreglo
 class TestLongitud(TestCase):
 
     def testElementosMinMax(self):
-        self.assertEqual(EstadisticaArreglo().obtenerLongitud("1,2"), [1, 1, 2], "cadena 1 elementos")
+        self.assertEqual(EstadisticaArreglo().obtenerLongitud("1,2,3,4"), [4, 1, 4], "cadena 1 elementos")
