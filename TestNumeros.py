e6e5e58a6a54f7ba8b68545cc541a3c033174e89
Hanner Andrés Ortiz <ha.ortiz11@uniandes.edu.co>
2018-09-30 21:46:31 -0500

    IT1 - Falla prueba de longitud N elementos (ROJO)

--- TestNumeros.py
+++ TestNumeros.py
@@ -7,7 +7,11 @@ class TestLongitud(TestCase):
 
 
     def TestLongitudConNumero(self):
-            self.assertEqual(EstadisticaArreglo().obtenerLongitud("1"), [1], "cadena con un numero")
+        self.assertEqual(EstadisticaArreglo().obtenerLongitud("1"), [1], "cadena con un numero")
 
     def TestLongitudDosNumero(self):
-            self.assertEqual(EstadisticaArreglo().obtenerLongitud("1,2"), [2], "cadena con dos numero")
+        self.assertEqual(EstadisticaArreglo().obtenerLongitud("1,2"), [2], "cadena con dos numero")
+
+    def TestLongitudNNumero(self):
+        self.assertEqual(EstadisticaArreglo().obtenerLongitud("1,2,3,4"), [4], "cadena con cuatro numero")
+        self.assertEqual(EstadisticaArreglo().obtenerLongitud("1,2,3,4,5"), [5], "cadena con cinco numero")
