90a925f06e9fcc22a8e9ba510acfd87811104c32
Hanner Andrés Ortiz <ha.ortiz11@uniandes.edu.co>
2018-09-30 23:36:14 -0500

    IT4- Prueba que devuelve Array con el número de elementos,el número minimo, maximo y promedio con cadena N de números (VERDE)

--- KataSimpleTDD.py
+++ KataSimpleTDD.py
@@ -8,4 +8,12 @@ class EstadisticaArreglo:
         if cadenaString == '':
             return [0, None, None, None]
         else:
-            return [len(arraySplit), int(min(arraySplit)), int(max(arraySplit))]
+
+            sum = 0.0
+            for i in range(0, len(arraySplit)):
+                sum = sum + int(arraySplit[i])
+            promedio = sum / len(arraySplit)
+
+            return [len(arraySplit), int(min(arraySplit)), int(max(arraySplit)),float(promedio)]
+
+
