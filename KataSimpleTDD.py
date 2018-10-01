504062e29c09df1c9631f3ceec5fd12034903d37
Hanner Andrés Ortiz <ha.ortiz11@uniandes.edu.co>
2018-09-30 22:52:58 -0500

    IT2 - Prueba que devuelve Array con el número de elementos y el número minimo con cadena de dos número (VERDE)

--- KataSimpleTDD.py
+++ KataSimpleTDD.py
@@ -2,8 +2,12 @@ import sys
 
 class EstadisticaArreglo:
     def obtenerLongitud(self, cadenaString):
+        arraySplit = cadenaString.split(',')
         if cadenaString == '':
             return [0, None]
-        else:
-            arraySplit = cadenaString.split(',')
-            return [len(arraySplit),int(cadenaString)]
+        elif len(cadenaString) >1:
+            if arraySplit[0] < arraySplit[1]:
+                return [len(arraySplit),int( arraySplit[0])]
+            else:
+                return [len(arraySplit), int(arraySplit[1])]
+
