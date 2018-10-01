dd4c2a920d331e06ae1791c6e8257727a0ee364b
Lady Pinzon <ljeraldynnepinzonc@gmail.com>
2018-09-30 23:27:13 -0500

    IT3 - Falla Prueba que devuelve el número de elementos,el minimo, el maximo y el promedio con cadena vacia de  elementos (ROJO)

--- TestNumeros.py
+++ TestNumeros.py
@@ -5,4 +5,4 @@ from KataSimpleTDD import EstadisticaArreglo
 class TestLongitud(TestCase):
 
     def testElementosMinMax(self):
-        self.assertEqual(EstadisticaArreglo().obtenerLongitud("1,2,3,4"), [4, 1, 4], "cadena 1 elementos")
+        self.assertEqual(EstadisticaArreglo().obtenerLongitud(""), [0, None, None,None], "cadena 0 elementos")
